beef846f7e2ca511438db8d99d442eba3f2232fc
harsheni10160 <devaharsheni2006@gmail.com>
2025-02-10 11:01:06 +0530

    sec commit

--- schema.js
+++ schema.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectDatabase = () => {
-  mongoose.connect(process.env.db_URL)
-  .then((data) =>{
-    console.log(`Connected to database: ${data.connection.host}`);
-})
-.catch((err) =>{
-    console.error(`Error connecting to database: ${err.message}`);
-});
-};
-
-module.exports = connectDatabase;
-
-
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
